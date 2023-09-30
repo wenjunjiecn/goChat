14e834269dd447c7b2f31f25de98832805434b15
wenjunjie <wenjunjiecn@163.com>
2023-09-30 16:37:21 +0800

    [ADD] 支持修改用户名

--- user.go
+++ user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -39,6 +42,20 @@ func (this *User) DoMessage(msg string) {
 			this.SendMessage(onlineMsg)
 		}
 		this.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.SendMessage("当前用户已经使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.SendMessage("您已经更新用户名：" + this.Name + "\n")
+		}
 	} else {
 		this.server.BroadCast(this, msg)
 	}
