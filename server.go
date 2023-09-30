28e5d2485888e3d8498636b73749b8a7b17d706f
wenjunjie <wenjunjiecn@163.com>
2023-09-30 15:31:58 +0800

    [ADD] 上线广播功能

--- server.go
+++ server.go
@@ -3,23 +3,58 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "已上线")
+
+	select {}
 }
 
 func (this *Server) Start() {
@@ -29,6 +64,8 @@ func (this *Server) Start() {
 		return
 	}
 	defer listener.Close()
+
+	go this.ListenMessage()
 
 	for {
 		conn, err := listener.Accept()
